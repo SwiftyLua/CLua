4112d28f9b0fb9c703f97dbca94cc5e44e8b9a05
Thomas Bonk <thomas@meandmymac.de>
2024-11-01 13:17:40 +0100

    adding OS dependent defines

--- Package.swift
+++ Package.swift
@@ -8,6 +8,19 @@ let package = Package(
 		.library(name: "CLua", type: .static, targets: ["CLua"]),
 	],
 	targets: [
-		.target(name: "CLua", dependencies: [])
+		.target(
+            name: "CLua",
+            dependencies: [],
+            #if os(macOS)
+            cSettings: [
+                .define("LUA_USE_MACOSX")
+            ]
+            #endif
+            #if os(Linux)
+            cSettings: [
+                .define("LUA_USE_LINUX")
+            ]
+            #endif
+        )
 	]
 )
