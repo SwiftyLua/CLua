af92193d0f54eed5c43ae70e7c7dc40b6c9d4ffd
Thomas Bonk <thomas@meandmymac.de>
2025-09-18 20:22:10 +0200

    Adjusted defines depending on the target platform.

--- Package.swift
+++ Package.swift
@@ -12,7 +12,10 @@ let package = Package(
             name: "CLua",
             dependencies: [],
             cSettings: [
-                .define("LUA_USE_MACOSX"),
+              .define("LUA_USE_MACOSX", .when(platforms: [.macOS])),
+              .define("LUA_USE_IOS", .when(platforms: [.iOS, .macCatalyst, .tvOS])),
+              .define("LUA_USE_LINUX", .when(platforms: [.linux])),
+              .define("LUA_USE_WINDOWS", .when(platforms: [.windows])),
             ]
         )
 	]
